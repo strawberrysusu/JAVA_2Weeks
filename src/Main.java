1874eaa0adf4025e83339cddde2fdfb7bc6a6669
CHO <202430312@email.daelim.ac.kr>
2024-09-11 17:18:32 +0900

    자동 커밋

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int a = 10;
+       double b = 1.1;
+       boolean c = true;
+       char d = 'c';
+       String e = "eqwe";
+        System.out.println(b);
+    }
+}
